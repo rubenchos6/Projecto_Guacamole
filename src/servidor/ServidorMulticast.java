caa8a664c82ac2f8b0fe47ad7655db7b768d3819
rubenchos6 <rubenrortega33@gmail.com>
2021-03-06 18:12:03 -0600

    Cambio en la funcion run para imprimir los resultados obtenidos en el servidor

--- src/servidor/ServidorMulticast.java
+++ src/servidor/ServidorMulticast.java
@@ -14,18 +14,20 @@ public class ServidorMulticast extends Thread{
     private static int MULTICAST_PORT=6868;
     private MulticastSocket socket;
     private InetAddress direccion;
-    private static String IP_MULTICAST= "225.228.225.228";
+    private static String IP_MULTICAST= "239.192.0.1";
     private static int CHANGE_POS_TIME=5000;
 
     public ServidorMulticast (JuegoGuacamole juego) {
         this.juego=juego;
     }
+
     public void despliegue(){
         try {
             this.socket = new MulticastSocket(MULTICAST_PORT);
             this.direccion=InetAddress.getByName(IP_MULTICAST);
             socket.joinGroup(direccion);
-
+            System.out.println("Servicio multicast desplegado");
+            this.start();
         } catch (IOException ex) {
             System.out.println("ServidorMulticast: Problemas para unirse al grupo.");
             Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
@@ -34,35 +36,35 @@ public class ServidorMulticast extends Thread{
 
     @Override
     public void run() {
-        while(!juego.getFinalizado()){
-            juego.mueveTopo();
-            String message = juego.obtenPosicion();
-            byte[] m= message.getBytes();
-            DatagramPacket messageOut= new DatagramPacket(m,m.length,direccion,MULTICAST_PORT);
+        while(true) {
+            while (!juego.getFinalizado()) {
+                juego.mueveTopo();
+                String message = "Pos: "+juego.obtenPosicion();
+                byte[] m = message.getBytes();
+                DatagramPacket messageOut = new DatagramPacket(m, m.length, direccion, MULTICAST_PORT);
+                try {
+                    socket.send(messageOut);
+                    Thread.sleep(CHANGE_POS_TIME);
+                } catch (IOException ex) {
+                    Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
 
-            try{
+            String message = "W: " + juego.getWinner();
+            System.out.println("JUEGO TERMINADO");
+            byte[] m = message.getBytes();
+            DatagramPacket messageOut = new DatagramPacket(m, m.length, direccion, MULTICAST_PORT);
+            juego.restart();
+            try {
                 socket.send(messageOut);
                 Thread.sleep(CHANGE_POS_TIME);
-            }catch (IOException ex){
+            } catch (IOException ex) {
                 Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
-            }catch (InterruptedException ex) {
+            } catch (InterruptedException ex) {
                 Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-
-        String message = "W: "+juego.getWinner();
-        byte[] m= message.getBytes();
-        DatagramPacket messageOut= new DatagramPacket(m,m.length,direccion,MULTICAST_PORT);
-
-        try{
-            socket.send(messageOut);
-            Thread.sleep(CHANGE_POS_TIME);
-        }catch (IOException ex){
-            Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
-        }catch (InterruptedException ex) {
-            Logger.getLogger(ServidorMulticast.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        juego.restart();
     }
 }

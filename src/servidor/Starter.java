1ab420faa8ec17929e798f62fe32388e946d183b
rubenchos6 <rubenrortega33@gmail.com>
2021-03-06 18:22:43 -0600

    Se agrega despliegue del servidor TCP

--- src/servidor/Starter.java
+++ src/servidor/Starter.java
@@ -5,12 +5,17 @@ public class Starter {
     public static void main (String args[]){
         //Inicia servidores RMI, Multicast y TCP
         // Tiene que iniciar el juego
+        System.setProperty("java.net.preferIPv4Stack", "true"); // Para el multicast
 
-        int n=3, m=3;
-        JuegoGuacamole juego= new JuegoGuacamole(n,m);
+        int n=9;
+        JuegoGuacamole juego= new JuegoGuacamole(n);
         ServidorRMI rmi= new ServidorRMI(juego);
         ServidorMulticast multi= new ServidorMulticast(juego);
+        ServidorTCP tcp = new ServidorTCP(juego);
 
+        rmi.despliegue();
+        multi.despliegue();
+        tcp.despliegue();
 
     }
 }

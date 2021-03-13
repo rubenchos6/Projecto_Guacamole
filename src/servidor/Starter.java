14d28a3b8c4e0d8d5d8e5267b7081451177851e4
rubenchos6 <rubenrortega33@gmail.com>
2021-03-13 16:23:39 -0600

    Propiedad para aumentar el maximo de sockets

--- src/servidor/Starter.java
+++ src/servidor/Starter.java
@@ -1,13 +1,16 @@
 package servidor;
 
+import estres.Globals;
+
 public class Starter {
 
     public static void main (String args[]){
         //Inicia servidores RMI, Multicast y TCP
         // Tiene que iniciar el juego
+        System.setProperty("sun.net.maxDatagramSockets", "500");
         System.setProperty("java.net.preferIPv4Stack", "true"); // Para el multicast
 
-        int n=9;
+        int n= Globals.topos;
         JuegoGuacamole juego= new JuegoGuacamole(n);
         ServidorRMI rmi= new ServidorRMI(juego);
         ServidorMulticast multi= new ServidorMulticast(juego);
